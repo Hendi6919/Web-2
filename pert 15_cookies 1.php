c42cb05fdb289dd4f49c54848bd5908eed64441c
Hendi6919 <80157299+Hendi6919@users.noreply.github.com>
2021-06-16 19:20:26 +0700

    Add files via upload

---
+++ pert 15_cookies 1.php
@@ -0,0 +1,10 @@
+<?php 
+$value = 'Hendi'; 
+$value2 = 'Hendi Uchiha'; 
+setcookie("username", $value); 
+setcookie("namalengkap", $value2, time()+3600); /* expire in 1
+hour */ 
+echo "<h1>Ini halaman pengesetan cookies</h1>"; 
+echo "<h2>Klik <a href='Pert15_cookies2.php'>di sini</a> untuk 
+pemeriksaan cookies</h2>"; 
+?> 
